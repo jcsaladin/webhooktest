d5b7665b8694c4339da78daa6603d4615259a364
Jean Carlos <jean.carlos.saladin@gmail.com>
2024-06-11 22:53:04 -0400

    testing

--- WebApplication4/Controllers/WeatherForecastController.cs
+++ WebApplication4/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApplication4.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Changing for the Good", "Another Change"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
